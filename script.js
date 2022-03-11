afe8e6edee3172ea05175775d59610b40569e354
AdmiralFirefox <jjlcuadradev@gmail.com>
2022-03-12 00:32:25 +0800

    Added Carousel Section

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
+// Initializing AOS
 AOS.init({
   once: true,
   duration: 2000,
   delay: 200,
+});
+
+// Initializing Swiper
+const swiper = new Swiper(".mySwiper", {
+  spaceBetween: 20,
+  loop: true,
+  speed: 1000,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
