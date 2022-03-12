d7d8203ab81f1f38d25aaf6616b599509fd21c2b
AdmiralFirefox <jjlcuadradev@gmail.com>
2022-03-12 15:02:54 +0800

    Added Carousel Section 2

--- script.js
+++ script.js
@@ -19,3 +19,23 @@ const swiper = new Swiper(".mySwiper", {
     clickable: true,
   },
 });
+
+const swiper2 = new Swiper(".mySwiper2", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    736: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
